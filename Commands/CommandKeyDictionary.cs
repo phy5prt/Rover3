73fb502e123c5c2b9f3f9d2e221cd2c20663ab3c
phy5prt <philip.r.tate@gmail.com>
2020-01-04 14:31:46 +0000

    functioning its working out next step

--- Commands/CommandKeyDictionary.cs
+++ Commands/CommandKeyDictionary.cs
@@ -8,6 +8,7 @@ namespace Rover3.Commands
     {
         //should this be in commands folder and static or should it be 
         //should it build itself
+        //https://stackoverflow.com/questions/5411694/get-all-inherited-classes-of-an-abstract-class
         public static Dictionary<string, Command> commandKeys = new Dictionary<string, Command>()
         {
             {"F", new MoveForward() },
